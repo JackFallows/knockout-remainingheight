ea4fbfe03b5932810fccdf42540320c6d96a30f1
Jack <fallows.jack@gmail.com>
2018-03-20 18:47:44 +0000

    fixed foreach loop

--- remainingHeight.js
+++ remainingHeight.js
@@ -1,20 +1,30 @@
-var updateHeight = function(element) {
-	var siblings = $(element).siblings();
-	var numHeightSum = 0;
-	siblings.forEach(function(sibling) {
-		var heightNum = ($(sibling).outerHeight(true));
-		numHeightSum += heightNum;
-	});
-
-	var elementHeight = $(element).parent().height() - numHeightSum;
-	
-	$(element).outerHeight("" + Math.round(elementHeight) + "px");
-}
-
-ko.bindingHandlers.remainingHeight = {
-	init: function(element) {
-		updateHeight(element);
-		
-		$(window).resize(updateHeight.bind(this, element));
+(function () {
+	if (ko == undefined) {
+		throw "Knockout is not defined.";
 	}
-}
+	
+	if ($ == undefined) {
+		throw "JQuery is not defined.";
+	}
+
+	var updateHeight = function (element) {
+		var siblings = $(element).siblings();
+		var numHeightSum = 0;
+		$(siblings).each(function (i, sibling) {
+			var heightNum = ($(sibling).outerHeight(true));
+			numHeightSum += heightNum;
+		});
+
+		var elementHeight = $(element).parent().height() - numHeightSum;
+		
+		$(element).outerHeight("" + Math.round(elementHeight) + "px");
+	}
+
+	ko.bindingHandlers.remainingHeight = {
+		init: function (element) {
+			updateHeight(element);
+			
+			$(window).resize(updateHeight.bind(this, element));
+		}
+	}
+})();
