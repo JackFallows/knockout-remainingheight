4a14f52ec612d44a9464f3d2122cb4c997bb3b2e
Jack Fallows <fallows.jack@gmail.com>
2018-09-09 13:59:43 +0100

    now supports binding to an observable to trigger recalculation

--- remainingHeight.js
+++ remainingHeight.js
@@ -8,7 +8,12 @@
 	}
 
 	ko.bindingHandlers.remainingHeight = {
-		init: function (element) {
+		update: function (element, valueAccessor) {
+		    const value = valueAccessor();
+		    if (ko.isObservable(value)) {
+		        value(); // this will apply a subscription to the observable, triggering a recalculation of the element height every time the observable's value changes
+            }
+            
 			$(element).css("height", "calc(100% - " + $(element).siblings(":visible").toArray().reduce(function (px, el) { return px += $(el).outerHeight(true) }, 0) + "px");
 		}
 	}
